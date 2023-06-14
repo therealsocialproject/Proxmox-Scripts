4be99a87b88b5c9c79b4e0280e68b65eee05e79a
Sam Wozencroft <45769279+samwozencroft@users.noreply.github.com>
2023-06-14 22:05:41 +0100

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
-# WARNING: This script will stop and remove all VMs and containers on your Proxmox host.
+# WARNING: This script will stop all VMs and containers and remove their configs on your Proxmox host.
 
-# Stop and remove all VMs
+# Stop all VMs
 for vmid in $(qm list | awk '{print $1}' | grep -o '[0-9]*' | grep -v '^$'); do
   echo "Stopping VM ID $vmid"
   qm stop $vmid --timeout 0
-  sleep 2
+done
+
+# Stop all containers and remove their configuration files
+for ctid in $(pct list | awk '{print $1}' | grep -o '[0-9]*' | grep -v '^$'); do
+  echo "Stopping container ID $ctid"
+  pct stop $ctid --timeout 0
+  echo "Removing container configuration ID $ctid"
+  rm -f /etc/pve/lxc/$ctid.conf
+done
+
+# Attempt to remove all VMs
+for vmid in $(qm list | awk '{print $1}' | grep -o '[0-9]*' | grep -v '^$'); do
   echo "Removing VM ID $vmid"
   qm destroy $vmid
 done
 
-# Stop and remove all containers
+# Attempt to remove all containers
 for ctid in $(pct list | awk '{print $1}' | grep -o '[0-9]*' | grep -v '^$'); do
-  echo "Stopping container ID $ctid"
-  pct stop $ctid --timeout 0
-  sleep 2
   echo "Removing container ID $ctid"
   pct destroy $ctid
 done
