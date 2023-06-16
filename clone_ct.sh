b811c10ba84e1afd7423fd713cd1a76a450cd7a0
Sam Wozencroft <45769279+samwozencroft@users.noreply.github.com>
2023-06-16 17:38:20 +0100

    Update clone_ct.sh

--- clone_ct.sh
+++ clone_ct.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-# Prompt for the container ID
-read -p "Enter the container ID to clone: " container_id
+# Prompt for the container ID or template
+read -p "Enter the container ID or template to clone: " container_id
 
-# Prompt for the number of copies
-read -p "Enter the number of copies to make: " num_copies
+# Prompt for the ID range for new cloned containers
+read -p "Enter the range of IDs for new cloned containers (e.g., 100-110): " id_range
+
+# Extract the start and end IDs from the range input
+start_id=$(echo "$id_range" | cut -d'-' -f1)
+end_id=$(echo "$id_range" | cut -d'-' -f2)
+
+# Determine if the provided ID is a template and convert it to a VM/CT ID
+if echo "$container_id" | grep -q "template"; then
+    container_id=$(pvesh get /cluster/nextid)
+fi
 
 # Loop to create clones
-for ((i=1; i<=num_copies; i++))
+for (( current_id=$start_id; current_id<=$end_id; current_id++ ))
 do
-    new_id="${container_id}_clone${i}"
-    echo "Cloning container ${container_id} to ${new_id}..."
-    pct clone ${container_id} ${new_id}
+    echo "Cloning container ${container_id} to ${current_id}..."
+    pct clone ${container_id} ${current_id} --full
 done
 
 echo "Cloning process completed."
